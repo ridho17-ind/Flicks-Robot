bcbf3532c59635e81e1d81335eb245484abfc92c
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 22:10:17 +0700

    Update nsfw.py

--- KanekiRobot/modules/nsfw.py
+++ KanekiRobot/modules/nsfw.py
@@ -4,14 +4,14 @@ import nekos
 import requests
 from PIL import Image
 from telegram import ParseMode
-from LaylaRobot import dispatcher, updater
-import LaylaRobot.modules.sql.nsfw_sql as sql
-from LaylaRobot.modules.log_channel import gloggable
+from Kaneki import dispatcher, updater
+import Kaneki.modules.sql.nsfw_sql as sql
+from Kaneki.modules.log_channel import gloggable
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
-from LaylaRobot.modules.helper_funcs.filters import CustomFilters
-from LaylaRobot.modules.helper_funcs.chat_status import user_admin
+from Kaneki.modules.helper_funcs.filters import CustomFilters
+from Kaneki.modules.helper_funcs.chat_status import user_admin
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 @run_async
@@ -870,4 +870,4 @@ __help__ = """
  - /dva: Sends Random D.VA source Images.
 """
 
-__mod_name__ = "NSFW"
+__mod_name__ = "ɴғsᴡ"
