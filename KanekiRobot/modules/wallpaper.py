f0cd12b3adc86a71f65ffb0b78e5fb1cf9f54461
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 22:29:19 +0700

    Update wallpaper.py

--- KanekiRobot/modules/wallpaper.py
+++ KanekiRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from LaylaRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from Kaneki import SUPPORT_CHAT, WALL_API, dispatcher
+from Kaneki.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
