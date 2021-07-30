7076c23f5f0eceb74ef4f1cc13472e7334566371
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 20:09:20 +0700

    Update nsfw_sql.py

--- KanekiRobot/modules/sql/nsfw_sql.py
+++ KanekiRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from LaylaRobot.modules.sql import BASE, SESSION
+from Kaneki.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
