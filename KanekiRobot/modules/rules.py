682c7d2c78a275832364dbffeb273ded580054fd
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 22:18:02 +0700

    Update rules.py

--- KanekiRobot/modules/rules.py
+++ KanekiRobot/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import LaylaRobot.modules.sql.rules_sql as sql
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import user_admin
-from LaylaRobot.modules.helper_funcs.string_handling import markdown_parser
+import Kaneki.modules.sql.rules_sql as sql
+from Kaneki import dispatcher
+from Kaneki.modules.helper_funcs.chat_status import user_admin
+from Kaneki.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
@@ -118,14 +118,14 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- ❍ /rules*:* get the rules for this chat.
+  /rules*:* get the rules for this chat.
 
 *Admins only:*
- ❍ /setrules <your rules here>*:* set the rules for this chat.
- ❍ /clearrules*:* clear the rules for this chat.
+  /setrules <your rules here>*:* set the rules for this chat.
+  /clearrules*:* clear the rules for this chat.
 """
 
-__mod_name__ = "Rules"
+__mod_name__ = "ʀᴜʟᴇs"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
