af0516dcde5eb9ad06df5edd09ba989ac7bf7c84
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 19:13:54 +0700

    Update __init__.py

--- KanekiRobot/__init__.py
+++ KanekiRobot/__init__.py
@@ -100,7 +100,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from Kaneki-ex.config import Development as Config
+    from Kaneki.config import Development as Config
 
     TOKEN = Config.TOKEN
 
@@ -200,7 +200,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Kaneki-ex.modules.helper_funcs.handlers import (
+from Kaneki.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
