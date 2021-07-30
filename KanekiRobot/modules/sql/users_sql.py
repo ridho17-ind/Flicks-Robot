5ae6ff00c0d2b62a2ef676a8b88a88ab84cb54df
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 20:11:32 +0700

    Update users_sql.py

--- KanekiRobot/modules/sql/users_sql.py
+++ KanekiRobot/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.sql import BASE, SESSION
+from Kaneki import dispatcher
+from Kaneki.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
