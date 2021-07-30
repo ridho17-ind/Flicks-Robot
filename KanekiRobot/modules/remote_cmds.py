6ac5330e8b570aaae047cfef3e06a99fd33d41c0
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-30 22:15:49 +0700

    Update remote_cmds.py

--- KanekiRobot/modules/remote_cmds.py
+++ KanekiRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from Kaneki import dispatcher
+from Kaneki.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from LaylaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from LaylaRobot.modules.helper_funcs.filters import CustomFilters
+from Kaneki.modules.helper_funcs.extraction import extract_user_and_text
+from Kaneki.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
