7fb30e599f6fa2f1397df6ae8049108de5174e87
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-31 00:22:42 +0700

    Update misc.py

--- KanekiRobot/modules/helper_funsc/misc.py
+++ KanekiRobot/modules/helper_funsc/misc.py
@@ -1,7 +1,7 @@
 from math import ceil
 from typing import Dict, List
 
-from LaylaRobot import NO_LOAD
+from Kaneki import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="layla_back"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("🔺", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("[ʙᴀᴄᴋ]", callback_data="kaneki_back"),
+             EqInlineKeyboardButton("🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
+        pairs += [[EqInlineKeyboardButton("[ʙᴀᴄᴋ]", callback_data="kaneki_back")]]
 
     return pairs
 
