fcf1abd163e62806523f358bb31c3db9e656b7e6
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 05:23:39 +0700

    Update shell.py

--- KanekiRobot/modules/shell.py
+++ KanekiRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from Kaneki import LOGGER, dispatcher
-from Kaneki.modules.helper_funcs.chat_status import dev_plus
+from KanekiRobot import LOGGER, dispatcher
+from KanekiRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
