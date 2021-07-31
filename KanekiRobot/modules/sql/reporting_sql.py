f135394060d1023e3330b7488a56f17d05e0b2b5
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:10:09 +0700

    Update reporting_sql.py

--- KanekiRobot/modules/sql/reporting_sql.py
+++ KanekiRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from Kaneki.modules.sql import BASE, SESSION
+from KanekiRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
