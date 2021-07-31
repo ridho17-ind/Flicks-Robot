2ce9a28f013a47bae813f37db6d10ea800de483e
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 05:41:37 +0700

    Update ud.py

--- KanekiRobot/modules/ud.py
+++ KanekiRobot/modules/ud.py
@@ -1,6 +1,6 @@
 import requests
-from Kaneki import dispatcher
-from Kaneki.modules.disable import DisableAbleCommandHandler
+from KanekiRobot import dispatcher
+from KanekiRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
