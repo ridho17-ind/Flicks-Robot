8fecf228b8ad733707a0576b8496923c899cc531
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-31 19:34:56 +0700

    Update pluginshelper.py

--- KanekiRobot/pyrogramee/pluginshelper.py
+++ KanekiRobot/pyrogramee/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from Kaneki  import OWNER_ID, SUPPORT_CHAT
-from Kaneki import pbot
+from KanekiRobot  import OWNER_ID, SUPPORT_CHAT
+from KanekiRobot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
