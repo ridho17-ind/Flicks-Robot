15f9126ae003ba02880ecbf47a8ecaed6046650f
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-07-31 13:35:49 +0700

    Update mongo.py

--- KanekiRobot/mongo.py
+++ KanekiRobot/mongo.py
@@ -10,14 +10,14 @@ from Kaneki.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_DB = "AnosVoldigoad"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["Kaneki-ex"]
+db = client["Kaneki-robot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
