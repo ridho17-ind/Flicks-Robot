c4b845b1e07ef145ab1f5a193e09af6c6f427f62
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-31 20:04:41 +0700

    Update extraction.py

--- KanekiRobot/modules/helper_funsc/extraction.py
+++ KanekiRobot/modules/helper_funsc/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Kaneki import LOGGER
-from Kaneki.modules.users import get_user_id
+from KanekiRobot import LOGGER
+from KanekiRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
