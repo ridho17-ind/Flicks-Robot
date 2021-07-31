6b6ea31780e5b2fee517050ed2a5c466fb15bf98
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 06:18:01 +0700

    Update __init__.py

--- KanekiRobot/__init__.py
+++ KanekiRobot/__init__.py
@@ -200,7 +200,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from KanekiRobot.modules.helper_funcs.handlers import (
+from KanekiRobot.modules.helper_funsc.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
