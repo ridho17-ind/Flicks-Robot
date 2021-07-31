772d1cda7b1ce7056fa4d0df156ba24263e8eaeb
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:51:58 +0700

    Update dev.py

--- KanekiRobot/modules/dev.py
+++ KanekiRobot/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import Kaneki
+import KanekiRobot
 
-from Kaneki import dispatcher
-from Kaneki.modules.helper_funcs.chat_status import dev_plus
+from KanekiRobot import dispatcher
+from KanekiRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
@@ -18,12 +18,12 @@ from telegram.ext import CallbackContext, CommandHandler, run_async
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {Kaneki.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"Current state: {KanekiRobot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
-        Kaneki.ALLOW_CHATS = True
+        KanekiRobot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
-        Kaneki.ALLOW_CHATS = False
+        KanekiRobot.ALLOW_CHATS = False
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
