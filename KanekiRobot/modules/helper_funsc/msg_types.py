5039f0a94493d577523b669cc91a86c05334228b
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 06:59:56 +0700

    Update msg_types.py

--- KanekiRobot/modules/helper_funsc/msg_types.py
+++ KanekiRobot/modules/helper_funsc/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from KanekiRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from KanekiRobot.modules.helper_funsc.string_handling import button_markdown_parser
 from telegram import Message
 
 
