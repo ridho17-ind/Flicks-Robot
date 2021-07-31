950012bedde1d3d573c96755bff9a7afb9cbd9d0
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-31 20:03:58 +0700

    Update chat_status.py

--- KanekiRobot/modules/helper_funsc/chat_status.py
+++ KanekiRobot/modules/helper_funsc/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from LaylaRobot import (
+from KanekiRobot import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from LaylaRobot.modules import connection
+from KanekiRobot.modules import connection
 
 connected = connection.connected
