d864c9a600c9ca89d0ef67655b960cd95a339e0b
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 06:34:54 +0700

    Update connection.py

--- KanekiRobot/modules/connection.py
+++ KanekiRobot/modules/connection.py
@@ -7,8 +7,8 @@ from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
 import KanekiRobot.modules.sql.connection_sql as sql
 from KanekiRobot import dispatcher, DRAGONS, DEV_USERS
-from KanekiRobot.modules.helper_funcs import chat_status
-from KanekiRobot.modules.helper_funcs.alternate import send_message, typing_action
+from KanekiRobot.modules.helper_funsc import chat_status
+from KanekiRobot.modules.helper_funsc.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
