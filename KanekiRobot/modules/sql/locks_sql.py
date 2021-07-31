11cb134dfad8ce96cc331448d4ebfeb7ca969972
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:08:30 +0700

    Update locks_sql.py

--- KanekiRobot/modules/sql/locks_sql.py
+++ KanekiRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from Kaneki.modules.sql import SESSION, BASE
+from KanekiRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
