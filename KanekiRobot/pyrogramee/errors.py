bc9545212ec47984fcd2bc186a3f01c06068f2f9
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-31 19:33:52 +0700

    Update errors.py

--- KanekiRobot/pyrogramee/errors.py
+++ KanekiRobot/pyrogramee/errors.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from functools import wraps
-from Kaneki import pbot, SUPPORT_CHAT
+from KanekiRobot import pbot, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
