97660fd95824bd3b740be14424d796888c041692
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:14:22 +0700

    Update night_mode_sql.py

--- KanekiRobot/modules/sql_extended/night_mode_sql.py
+++ KanekiRobot/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from Kaneki.modules.sql import BASE, SESSION
+from KanekiRobot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
