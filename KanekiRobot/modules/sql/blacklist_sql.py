b566fec3ba9656b61998a1839098798c39b1a147
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:03:17 +0700

    Update blacklist_sql.py

--- KanekiRobot/modules/sql/blacklist_sql.py
+++ KanekiRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from Kaneki.modules.sql import SESSION, BASE
+from KanekiRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
