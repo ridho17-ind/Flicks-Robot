dc90ceccb679bb46b00028277c43bcf81df058d1
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:30:39 +0700

    Update __weather.py

--- KanekiRobot/modules/__weather.py
+++ KanekiRobot/modules/__weather.py
@@ -1,4 +1,4 @@
-from Kaneki import telethn as tbot
+from KanekiRobot import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from Kaneki import *
+from KanekiRobot import *
 
-from Kaneki.events import register
+from KanekiRobot.events import register
 
 
 async def is_register_admin(chat, user):
