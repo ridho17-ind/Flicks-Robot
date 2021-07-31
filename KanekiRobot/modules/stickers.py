b86265648818f6a5197e344b7d698b753c5f163b
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 05:26:22 +0700

    Update stickers.py

--- KanekiRobot/modules/stickers.py
+++ KanekiRobot/modules/stickers.py
@@ -12,8 +12,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from Kaneki import dispatcher
-from Kaneki.modules.disable import DisableAbleCommandHandler
+from KanekiRobot import dispatcher
+from KanekiRobot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
