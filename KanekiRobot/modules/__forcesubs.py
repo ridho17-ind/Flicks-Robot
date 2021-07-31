bc154f2c2f4892ee9cda56820475da372edfa184
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:19:11 +0700

    Update __forcesubs.py

--- KanekiRobot/modules/__forcesubs.py
+++ KanekiRobot/modules/__forcesubs.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from Kaneki import DRAGONS as SUDO_USERS
-from Kaneki import pbot
-from Kaneki.modules.sql_extended import forceSubscribe_sql as sql
+from KanekiRobot import DRAGONS as SUDO_USERS
+from KanekiRobot import pbot
+from KanekiRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
