66d3e9c0c285f0b39f42a7dd2ebb5d1250940686
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:55:45 +0700

    Update error_handler.py

--- KanekiRobot/modules/error_handler.py
+++ KanekiRobot/modules/error_handler.py
@@ -9,7 +9,7 @@ import pretty_errors
 import io
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
-from Kaneki import dispatcher, DEV_USERS, OWNER_ID
+from KanekiRobot import dispatcher, DEV_USERS, OWNER_ID
 
 pretty_errors.mono()
 
