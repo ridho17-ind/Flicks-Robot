bae27e87394175878cd91a6e6a73f4026b1a8268
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:50:54 +0700

    Update debug.py

--- KanekiRobot/modules/debug.py
+++ KanekiRobot/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from Kaneki import telethn, dispatcher
-from Kaneki.modules.helper_funcs.chat_status import dev_plus
+from KanekiRobot import telethn, dispatcher
+from KanekiRobot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
