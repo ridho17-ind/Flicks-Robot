77b831a88467e67704d9350256c8b4fffba5600f
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 04:36:03 +0700

    Update afk.py

--- KanekiRobot/modules/afk.py
+++ KanekiRobot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from Kaneki import dispatcher
-from Kaneki.modules.disable import (
+from KanekiRobot import dispatcher
+from KanekiRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from Kaneki.modules.sql import afk_sql as sql
-from Kaneki.modules.users import get_user_id
+from KanekiRobot.modules.sql import afk_sql as sql
+from KanekiRobot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
