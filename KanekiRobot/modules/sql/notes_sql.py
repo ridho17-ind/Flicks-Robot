7932f4e9a6e8b3801c9353d91cc9fa42fb656624
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 08:07:42 +0700

    Update notes_sql.py

--- KanekiRobot/modules/sql/notes_sql.py
+++ KanekiRobot/modules/sql/notes_sql.py
@@ -1,7 +1,7 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from KanekiRobot.modules.helper_funcs.msg_types import Types
+from KanekiRobot.modules.helper_funsc.msg_types import Types
 from KanekiRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
