09a0dbb76fb20a0f58843f83edf182e8a3de8936
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 07:17:41 +0700

    Update dev.py

--- KanekiRobot/modules/dev.py
+++ KanekiRobot/modules/dev.py
@@ -8,7 +8,7 @@ from time import sleep
 import KanekiRobot
 
 from KanekiRobot import dispatcher
-from KanekiRobot.modules.helper_funcs.chat_status import dev_plus
+from KanekiRobot.modules.helper_funsc.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
