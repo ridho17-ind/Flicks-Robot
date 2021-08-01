22e9619b5582044d9dc99ff744c3b343427126bb
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-02 03:49:29 +0700

    Update __init__.py

--- KanekiRobot/__init__.py
+++ KanekiRobot/__init__.py
@@ -21,8 +21,8 @@ LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    sys.exit(1)
     )
     quit(1)
 
