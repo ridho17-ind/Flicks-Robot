3ef76442f79cbccb28785bfd59cf75db056f0deb
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-01 23:30:40 +0700

    Update misc.py

--- KanekiRobot/modules/helper_funsc/misc.py
+++ KanekiRobot/modules/helper_funsc/misc.py
@@ -66,9 +66,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔺", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("[ʙᴀᴄᴋ]", callback_data="Kaneki_back"),
-             EqInlineKeyboardButton("🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("[ ʙᴀᴄᴋ ]", callback_data="Kaneki_back"),
+             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("[ʙᴀᴄᴋ]", callback_data="kaneki_back")]]
