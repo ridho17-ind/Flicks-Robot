b88ca991270b598804ac0437974320959659d00c
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-02 05:07:13 +0700

    Create repo.py

---
+++ KanekiRobot/modules/repo.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+from KanekiRobot import pbot as app
+from KanekiRobot.utils.errors import capture_err
+from KanekiRobot.utils.http import get
+
+
+@app.on_message(filters.command("repo") & ~filters.edited)
+@capture_err
+async def repo(_, message):
+    users = await get(
+        "https://api.github.com/repos/rizkyoye/Kaneki-robot/contributors"
+    )
+    list_of_users = ""
+    count = 1
+    for user in users:
+        list_of_users += (
+            f"**{count}.** [{user['login']}]({user['html_url']})\n"
+        )
+        count += 1
+
+    text = f"""[Bot](https://t.me/kanekiexbot) | [Support](https://t.me/kanekisupport)
+```----------------
+| Contributors |
+----------------```
+{list_of_users}"""
+    await app.send_message(
+        message.chat.id, text=text, disable_web_page_preview=True
+    )
