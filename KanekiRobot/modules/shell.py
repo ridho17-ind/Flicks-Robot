8bc85bd22da58fbe7fba8b901ff1fa9e3fe16464
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-02 01:53:15 +0700

    Update shell.py

--- KanekiRobot/modules/shell.py
+++ KanekiRobot/modules/shell.py
@@ -45,6 +45,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh"], shell)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "Shell"
+__mod_name__ = "sʜᴇʟʟ"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
