f86853826b916ee4700ec9217461373ae23895b3
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-08-02 17:01:12 +0700

    Update afk.py

--- KanekiRobot/modules/afk.py
+++ KanekiRobot/modules/afk.py
@@ -132,7 +132,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             res = "{} is afk".format(fst_name)
             update.effective_message.reply_text(res)
         else:
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} Sedang Afk.\nAlasan: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
